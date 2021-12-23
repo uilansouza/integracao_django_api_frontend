c10182fc85104156866abe82088cb2c96745a66d
Uilan <uilan.souza@hotmail.com>
2021-12-23 12:21:26 -0300

    pronto

---
+++ Api/escola/views.py
@@ -0,0 +1,62 @@
+from rest_framework import status, viewsets, generics
+from rest_framework.serializers import Serializer
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, AlunoSerializerV2, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer
+from rest_framework.response import Response
+
+#from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+   # authentication_classes = [BasicAuthentication] -- default REST_FRAMEWORK  definida no settgins
+   # permission_classes = [IsAuthenticated, DjangoModelPermissions] -- default REST_FRAMEWORK  definida no settgins
+
+    def get_serializer_class(self):
+     if self.request.version == 'v2':
+         return AlunoSerializerV2
+     else:
+         return AlunoSerializer
+
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+    def create(self, request):
+        """  Esse Create é usado para documentar a APi.
+         Aparece no networking do navegador o nome do recurso
+         http://localhost/cursos/6
+        """
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location']=request.build_absolute_uri() + id
+            return response
+
+   
+
+class MatriculaViewSet(viewsets.ModelViewSet):
+    """Listando todas as matrículas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+    http_method_names =['get','post','put','path']
+  
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
+  
+class ListaAlunosMatriculados(generics.ListAPIView):
+    """Listando alunos e alunas matriculados em um curso"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(curso_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaAlunosMatriculadosSerializer
+  
